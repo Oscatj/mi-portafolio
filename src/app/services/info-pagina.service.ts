ba16bee04b7795c5a76cf79686c64b8f283369df
Oscatj <97744594+Oscatj@users.noreply.github.com>
2022-06-29 23:40:24 -0400

    creacion de base de datos y carga de datos a about

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,21 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pag.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
-  info: any = {};
+  info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
-    
+      this.cargarInfo();
+      this.cargarEquipo();
+   }
+
+  private cargarInfo(){
+
     this.http.get('assets/data/dataInfo.json')
-    .subscribe( resp => {
+    .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
+    });
+
+  }
+   private cargarEquipo(){
+    this.http.get('https://portafolio-angular-63d4c-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+
+      this.equipo = resp;
       console.log(resp);
     });
-    console.log('Servicio cargado correctamente');
-   }
+  }
 }
