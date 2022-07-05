996ea84251b8b541509d4cd9bb55e666fb15a379
Oscatj <97744594+Oscatj@users.noreply.github.com>
2022-07-05 18:32:33 -0400

    boton descarga

--- src/app/pages/portafolio/portafolio.component.ts
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -10,8 +10,16 @@ export class PortafolioComponent implements OnInit {
 
   constructor(public productosServices: ProductosService) { }
 
-
    ngOnInit(): void {
-              }           
+              }        
+   descarga(path: string = '/assets/cv.pdf'){
+    let link = document.createElement('a');
+    link.setAttribute('type', 'hidden');
+    link.href = 'assets/file';
+    link.download = path;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+   }   
 
 }
